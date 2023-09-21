a4b60171a250fb485d256854470b7e63e6cdcda4
Mr Kara <22439498+mrkaraly@users.noreply.github.com>
2023-09-21 21:40:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,25 +9,27 @@
 
 int main() {
     char input[MAX_INPUT_LENGTH];
+    ssize_t input_length;
 
     while (1) {
-        // Display the prompt
+        /* Display the prompt */
         write(STDOUT_FILENO, "simple_shell> ", 14);
 
-        // Read user input
-        ssize_t input_length = read(STDIN_FILENO, input, sizeof(input));
+        /* Read user input */
+        input_length = read(STDIN_FILENO, input, sizeof(input));
         if (input_length == -1) {
-            // Handle EOF (Ctrl+D)
+            /* Handle EOF (Ctrl+D) */
             write(STDOUT_FILENO, "\nExiting simple_shell.\n", 24);
             break;
         }
 
-        // Null-terminate the input string
+        /* Null-terminate the input string */
         input[input_length] = '\0';
 
-        // Remove the newline character
+        /* Remove the newline character */
         int newline_pos = -1;
-        for (int i = 0; i < input_length; i++) {
+        int i;
+        for (i = 0; i < input_length; i++) {
             if (input[i] == '\n') {
                 newline_pos = i;
                 break;
@@ -37,20 +39,23 @@ int main() {
             input[newline_pos] = '\0';
         }
 
-        // Fork a child process
-        pid_t pid = fork();
+        /* Fork a child process */
+        pid_t pid;
+        pid = fork();
 
         if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
+        }
+
+        if (pid == 0) {
+            /* Child process */
             char *args[] = {input, NULL};
             execve(input, args, NULL);
             perror("execve");
-            exit(EXIT_FAILURE);
+            _exit(EXIT_FAILURE);
         } else {
-            // Parent process
+            /* Parent process */
             int status;
             if (waitpid(pid, &status, 0) == -1) {
                 perror("waitpid");
